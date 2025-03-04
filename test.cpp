da20fd064f006407c2ebf7651b4b746ea4767610
NgocAnh <lethingocanhh2808@gmail.com>
2025-03-04 23:46:19 +0700

    Revert "commit 6"
    This reverts commit 5cad0cbd78cde1f5eaf91b32da00e79b6d323b7d.

--- test.cpp
+++ test.cpp
@@ -7,15 +7,5 @@ int main() {
     cout << "I'm a student." << endl;
     cout << "I'm a dog." << endl;
     cout << "I'm a cat." << endl;
-    cout << "I'm a bird." << endl;
-    cout << "I'm a fish." << endl;
-    cout << "I'm a monkey." << endl;
-    cout << "I'm studying at HUST." << endl;
-    cout << "I'm studying at Hanoi University of Science and Technology." << endl;
-    cout << "I'm studying at Hanoi University of Science and Technology (HUST)." << endl;
-    cout << "I'm studying at Hanoi University of Science and Technology (HUST) in Vietnam." << endl;
-    cout << "I'm a pig." << endl;
-    cout << "I'm a cow." << endl;
-    cout << "I'm a horse." << endl;
     return 0;
 }
