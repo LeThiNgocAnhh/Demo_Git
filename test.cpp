526de57628571fe96bc8538964010b9644faa249
NgocAnh <lethingocanhh2808@gmail.com>
2025-03-04 22:58:44 +0700

    commit 4

--- test.cpp
+++ test.cpp
@@ -4,5 +4,6 @@ int main() {
     cout << "Hello, World!" << endl;
     cout << "This is a test program." << endl;
     cout << "Demo_Git" << endl;
+    cout << "I'm a student." << endl;
     return 0;
 }
