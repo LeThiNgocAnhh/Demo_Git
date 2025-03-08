1b56923ee170c6fd5ffc60644e167029eb72db9d
NgocAnh <lethingocanhh2808@gmail.com>
2025-03-08 22:22:56 +0700

    Revert "commit 2.1 branch main"
    This reverts commit e6a24d43b73f4eebdb2596bcb128a555545c394d.

--- demo.cpp
+++ demo.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int main(){
     cout << "Clone code"<< endl;
     cout << "Push code in github"<< endl; 
-    cout << "Create conflicts in local" << endl;
     cout << "Create conflicts" <<endl;
     cout << "Create branch ngocanh" << endl;
     cout << "Pull code" << endl;
