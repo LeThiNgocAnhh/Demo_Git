d7d37dc1cc07efd412608b15a69baf5c8e875c94
NgocAnh <lethingocanhh2808@gmail.com>
2025-03-08 22:16:00 +0700

    commit 2 branch ngocanh

--- demo.cpp
+++ demo.cpp
@@ -7,5 +7,6 @@ int main(){
     cout << "Create conflicts" <<endl;
     cout << "Create branch ngocanh" << endl;
     cout << "Pull code" << endl;
+    cout << "Create conflicts branch ngocanh vs anh"<< endl;
     return 0;
 }
